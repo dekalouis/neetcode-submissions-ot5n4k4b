bfba6caadf23fa8cb80a98a7cbe5e2f88a99e5ba
Deka Louis <83022812+dekalouis@users.noreply.github.com>
2026-08-18 17:29:56 +0700

    Add: anagram-groups - submission-27

---
+++ Data Structures & Algorithms/anagram-groups/submission-27.py
@@ -0,0 +1,20 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = {}
+
+        for s in strs:
+            freq = [0] * 26
+
+            for c in s:
+                idx = ord(c) - ord('a')
+                freq[idx] += 1
+            
+            # print(freq)
+            sign = tuple(freq)
+
+            if sign not in res:
+                res[sign] = []
+            res[sign].append(s)
+
+            # print(res)
+        return list(res.values())

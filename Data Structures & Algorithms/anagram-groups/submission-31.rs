f4c3e81a977fe16ebffe5908f81823b764b00b59
Deka Louis <83022812+dekalouis@users.noreply.github.com>
2026-08-28 01:15:28 +0700

    Add: anagram-groups - submission-31

---
+++ Data Structures & Algorithms/anagram-groups/submission-31.rs
@@ -0,0 +1,14 @@
+impl Solution {
+    pub fn group_anagrams(strs: Vec<String>) -> Vec<Vec<String>> {
+        let mut res: HashMap<[u8; 26], Vec<String>> = HashMap::new();
+        for s in &strs {
+            let mut count = [0u8; 26];
+            for c in s.bytes() {
+                let mut idx = (c - b'a') as usize;
+                count[idx] += 1;
+            }
+            res.entry(count).or_default().push(s.clone());
+        }
+        res.into_values().collect()
+    }
+}
